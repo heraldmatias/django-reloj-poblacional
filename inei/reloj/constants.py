cd412e33d8680e3a7cb145491885f3fd71246bed
heraldmatias <heraldmatias.oz@gmail.com>
2014-02-24 17:24:05 -0500

    first commit

--- inei/reloj/constants.py
+++ inei/reloj/constants.py
@@ -2,6 +2,6 @@ __author__ = 'holivares'
 from datetime import datetime
 
 POPULATION = 30673918
-COUNTER = 193
+COUNTER = 93
 YEAR = 2014
 START_DATE = datetime.strptime('31/01/2014 00:00:00','%d/%m/%Y %H:%M:%S')
