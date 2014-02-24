a8cfdf6c3aad9e1a42847843b08288caad2f6eda
heraldmatias <heraldmatias.oz@gmail.com>
2014-02-24 17:05:35 -0500

    first contants

--- inei/reloj/constants.py
+++ inei/reloj/constants.py
@@ -1,7 +1,7 @@
 __author__ = 'holivares'
 from datetime import datetime
 
-POPULATION = 30151500
+POPULATION = 30673918
 COUNTER = 193
-YEAR=2014
+YEAR = 2014
 START_DATE = datetime.strptime('31/01/2014 00:00:00','%d/%m/%Y %H:%M:%S')
