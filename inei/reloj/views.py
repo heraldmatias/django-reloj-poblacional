8989fd705fdea3f9b2d9ae7a97bdbc5f142fecf2
heraldmatias <heraldmatias.oz@gmail.com>
2014-02-26 18:07:01 -0500

    first commit

--- inei/reloj/views.py
+++ inei/reloj/views.py
@@ -22,8 +22,8 @@ class PopulationView(TemplateView):
 
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
-        # if not request.is_ajax():
-        #     raise Http404("Only ajax.")
+        if not request.is_ajax():
+            raise Http404("Only ajax.")
         return super(PopulationView, self).dispatch(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
