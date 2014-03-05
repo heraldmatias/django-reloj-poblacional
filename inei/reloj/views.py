c49a29b7c1b571ab97d2b96e089cc14d3b7eacbe
heraldmatias <heraldmatias.oz@gmail.com>
2014-03-05 15:59:38 -0500

    reloj json

--- inei/reloj/views.py
+++ inei/reloj/views.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import json
 from django.views.generic import TemplateView
 from django.http.response import Http404, HttpResponse
 from django.utils.decorators import method_decorator
@@ -24,12 +25,26 @@ class PopulationView(TemplateView):
 
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
-        if not request.is_ajax():
-            raise Http404("Only ajax.")
+        # if not request.is_ajax():
+        #     raise Http404("Only ajax.")
         return super(PopulationView, self).dispatch(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
         api = Api(2014)
-        population = api.get_estimated_population()
-        response = HttpResponse(population, content_type="text/html")
+        data = {
+            'elapsed_time': api.get_elapsed_time(),
+            'population': api.get_estimated_population(),
+            'success': True
+        }
+        response = HttpResponse(json.dumps(data), content_type="application/json")
+        return response
+
+    def get(self, request, *args, **kwargs):
+        api = Api(2014)
+        data = {
+            'elapsed_time': api.get_elapsed_time(),
+            'population': api.get_estimated_population(),
+            'success': True
+        }
+        response = HttpResponse(json.dumps(data), content_type="application/json")
         return response
